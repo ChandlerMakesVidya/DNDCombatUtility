b18809b10f9641942e0a10b6d429d9da27daccaf
ChandlerMakesVidya <71201464+ChandlerMakesVidya@users.noreply.github.com>
2023-07-17 20:26:15 -0500

    Table now self sorts by descending initiative.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const creatureNameInput = document.getElementById("creatureName");
 const healthInput = document.getElementById("health");
 const effectsInput = document.getElementById("effects");
 
+$("#inputForm").submit(function(e) {
+    e.preventDefault();
+});
+
 function addRow() {
     let newRow = table.insertRow(-1);
     newRow.insertCell(0).innerHTML = initiativeCountInput.value;
@@ -16,4 +20,37 @@ function addRow() {
 
 function clearInputs() {
     inputForm.reset();
+    initiativeCountInput.focus();
 }
+
+function sortTable() {
+    let rows, switching, i, x, y, shouldSwitch;
+    switching = true;
+    while(switching) {
+        switching = false;
+        rows = table.rows;
+        console.log(rows);
+        for(i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[0];
+            y = rows[i+1].getElementsByTagName("td")[0];
+            if(x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if(shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+// testing if jquery works
+/*window.onload = () => {
+    if (window.jQuery) {
+        alert("YEAH!");
+    } else {
+        alert("SHIT");
+    }
+}*/
