c4242bf019da983ff3f1e59375ea23288fed3f48
ChandlerMakesVidya <71201464+ChandlerMakesVidya@users.noreply.github.com>
2023-08-04 12:59:10 -0500

    Hopefully stopped the page from refreshing

--- index.js
+++ index.js
@@ -6,50 +6,55 @@ const healthInput = document.getElementById("health");
 const effectsInput = document.getElementById("effects");
 const inputErrorMessage = document.getElementById("inputErrorMessage");
 
-$("#inputForm").submit(function(e) {
-    e.preventDefault();
-});
+inputForm.addEventListener("submit", stopRefresh, false);
+
+function stopRefresh(event) {
+  event.preventDefault();
+}
 
 function addRow() {
-    inputErrorMessage.style.opacity = 0;
-    if(isNaN(Number(initiativeCountInput.value)) || isNaN(Number(healthInput.value))){
-        inputErrorMessage.style.opacity = 1;
-        return;
-    }
+  inputErrorMessage.style.opacity = 0;
+  if (
+    isNaN(Number(initiativeCountInput.value)) ||
+    isNaN(Number(healthInput.value))
+  ) {
+    inputErrorMessage.style.opacity = 1;
+    return;
+  }
 
-    let newRow = table.insertRow(-1);
-    newRow.insertCell(0).innerHTML = initiativeCountInput.value;
-    newRow.insertCell(1).innerHTML = creatureNameInput.value;
-    newRow.insertCell(2).innerHTML = healthInput.value;
-    newRow.insertCell(3).innerHTML = effectsInput.value;
-    newRow.insertCell(4).innerHTML = "-";
+  let newRow = table.insertRow(-1);
+  newRow.insertCell(0).innerHTML = initiativeCountInput.value;
+  newRow.insertCell(1).innerHTML = creatureNameInput.value;
+  newRow.insertCell(2).innerHTML = healthInput.value;
+  newRow.insertCell(3).innerHTML = effectsInput.value;
+  newRow.insertCell(4).innerHTML = "-";
 
-    inputForm.reset();
-    initiativeCountInput.focus();
-    sortTable();
+  inputForm.reset();
+  initiativeCountInput.focus();
+  sortTable();
 }
 
 function sortTable() {
-    let rows, switching, i, x, y, shouldSwitch;
-    switching = true;
-    while(switching) {
-        switching = false;
-        rows = table.rows;
-        console.log(rows);
-        for(i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false;
-            x = Number(rows[i].getElementsByTagName("td")[0].innerHTML);
-            y = Number(rows[i+1].getElementsByTagName("td")[0].innerHTML);
-            if(x < y) {
-                shouldSwitch = true;
-                break;
-            }
-        }
-        if(shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
-            switching = true;
-        }
+  let rows, switching, i, x, y, shouldSwitch;
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    console.log(rows);
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = Number(rows[i].getElementsByTagName("td")[0].innerHTML);
+      y = Number(rows[i + 1].getElementsByTagName("td")[0].innerHTML);
+      if (x < y) {
+        shouldSwitch = true;
+        break;
+      }
     }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
 }
 
 // testing if jquery works
